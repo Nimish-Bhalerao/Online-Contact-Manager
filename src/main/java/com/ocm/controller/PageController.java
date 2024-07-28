3540d1641aabbe17d8849f87e59e8153d59d67e2
Nimish Bhalerao <nimishbhalerao123@gmail.com>
2024-07-28 18:39:59 +0530

    2nd commit

--- src/main/java/com/ocm/controller/PageController.java
+++ src/main/java/com/ocm/controller/PageController.java
@@ -77,6 +77,8 @@ private UserService userService;
     //     .profilePic("https://media.istockphoto.com/id/610003972/vector/vector-businessman-black-silhouette-isolated.jpg?s=612x612&w=0&k=20&c=Iu6j0zFZBkswfq8VLVW8XmTLLxTLM63bfvI6uXdkacM=")
     // .build();
 
+    //test for git 
+
     User user = new User();
     user.setName(userForm.getName());
     user.setAbout(userForm.getAbout());
