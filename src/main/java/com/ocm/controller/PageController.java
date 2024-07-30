f60dfb49f514340a0de3abcee76d4d041d3f19dd
Nimish Bhalerao <nimishbhalerao123@gmail.com>
2024-07-30 08:07:21 +0530

    Registeration sucessfull msg displayed only once

--- src/main/java/com/ocm/controller/PageController.java
+++ src/main/java/com/ocm/controller/PageController.java
@@ -3,12 +3,11 @@ package com.ocm.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-
 
 import com.ocm.entites.User;
 import com.ocm.forms.UserForm;
@@ -17,13 +16,13 @@ import com.ocm.helpers.MessageType;
 import com.ocm.services.UserService;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class PageController {
-@Autowired
-private UserService userService;
+    @Autowired
+    private UserService userService;
 
-    
     @RequestMapping("/home")
     public String home(Model model) {
         System.out.println("Home page handler");
@@ -60,39 +59,49 @@ private UserService userService;
     @GetMapping("/register")
     public String register(Model model) {
         UserForm userForm = new UserForm();
-       // userForm.setName("Nimish");
-       // userForm.setEmail("nimish@gmail.com");
+        // userForm.setName("Nimish");
+        // userForm.setEmail("nimish@gmail.com");
         model.addAttribute("userForm", userForm);
+
         return "register";
     }
 
-    @RequestMapping(value = "/do-register",method = RequestMethod.POST )
-    public String processRegister(@ModelAttribute UserForm userForm,HttpSession session) {
+    @RequestMapping(value = "/do-register", method = RequestMethod.POST)
+    public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult rbindingResult, HttpSession session) {
         System.out.println("Processing register");
-      System.out.println(userForm);
-    //   User user = User.builder()
-    //           .name(userForm.getName())
-    //           .email(userForm.getEmail())
-    //           .password(userForm.getPassword())
-    //           .about(userForm.getAbout())
-    //           .phoneNumber(userForm.getPhoneNumber())
-    //     .profilePic("https://media.istockphoto.com/id/610003972/vector/vector-businessman-black-silhouette-isolated.jpg?s=612x612&w=0&k=20&c=Iu6j0zFZBkswfq8VLVW8XmTLLxTLM63bfvI6uXdkacM=")
-    // .build();
+        System.out.println(userForm);
 
-    //test for git 
+        // validating form data
 
-    User user = new User();
-    user.setName(userForm.getName());
-    user.setAbout(userForm.getAbout());
-    user.setEmail(userForm.getEmail());
-    user.setPhoneNumber(userForm.getPhoneNumber());
-    user.setPassword(userForm.getPassword());
-    user.setProfilePic("https://media.istockphoto.com/id/610003972/vector/vector-businessman-black-silhouette-isolated.jpg?s=612x612&w=0&k=20&c=Iu6j0zFZBkswfq8VLVW8XmTLLxTLM63bfvI6uXdkacM=");
+        if (rbindingResult.hasErrors()) {
+            System.out.println("Form has errors");
+            return "register";
+        }
 
-      User savedUser = userService.saveUser(user);
-      System.out.println("Saved user: ");
-      Message message = Message.builder().content("Registration sucessfull!!").type(MessageType.green).build();
-       session.setAttribute("message",message);
+        // User user = User.builder()
+        // .name(userForm.getName())
+        // .email(userForm.getEmail())
+        // .password(userForm.getPassword())
+        // .about(userForm.getAbout())
+        // .phoneNumber(userForm.getPhoneNumber())
+        // .profilePic("https://media.istockphoto.com/id/610003972/vector/vector-businessman-black-silhouette-isolated.jpg?s=612x612&w=0&k=20&c=Iu6j0zFZBkswfq8VLVW8XmTLLxTLM63bfvI6uXdkacM=")
+        // .build();
+
+        // test for git
+
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setAbout(userForm.getAbout());
+        user.setEmail(userForm.getEmail());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setPassword(userForm.getPassword());
+        user.setProfilePic(
+                "https://media.istockphoto.com/id/610003972/vector/vector-businessman-black-silhouette-isolated.jpg?s=612x612&w=0&k=20&c=Iu6j0zFZBkswfq8VLVW8XmTLLxTLM63bfvI6uXdkacM=");
+
+        User savedUser = userService.saveUser(user);
+        System.out.println("Saved user: ");
+        Message message = Message.builder().content("Registration sucessfull!!").type(MessageType.green).build();
+        session.setAttribute("message", message);
         return "redirect:/register";
     }
 }
--- src/main/java/com/ocm/forms/UserForm.java
+++ src/main/java/com/ocm/forms/UserForm.java
@@ -1,5 +1,8 @@
 package com.ocm.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,9 +17,22 @@ import lombok.ToString;
 @Builder
 @ToString
 public class UserForm {
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+
+    @Email(message = "Invalid email address")
+    @NotBlank(message = "Email cannot be blank")
     private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 100, message = "Password must be of minimum 6 characters")
     private String password;
+
+    @NotBlank(message = "About cannot be blank")
     private String about;
+
+    @Size(min = 10, max = 10, message = "Phone number must be of 10 digits")
     private String phoneNumber;
 }
